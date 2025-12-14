4b1255773c86951c28c71b903cbb26cc26996348
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-14 19:56:09 +0530

    Update S3 bucket policy to use new syntax

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -24,11 +24,13 @@ resource "aws_s3_bucket_policy" "website" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.website.arn}/*"
-    }]
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
   })
 }
