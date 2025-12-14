4ef9ff4a5832c22213ec9f6db994a7df7d9350b5
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-14 20:05:35 +0530

    Add S3 bucket public access block resource

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -19,10 +19,19 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
-    policy = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
