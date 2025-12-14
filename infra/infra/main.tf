cc49559e514eb0dcd3a2ad044e606816f9857673
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-14 20:03:18 +0530

    Complete S3 bucket policy and fix infra structure

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_public_access_block" "website" {
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
-  policy = jsonencode({
+    policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
