219e12715459c7ec19c59951fca27aff49a52fcb
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-14 19:52:14 +0530

    Modify S3 bucket settings and add configurations
    Updated S3 bucket configuration and added public access block and policy.

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -1,8 +1,34 @@
 resource "aws_s3_bucket" "website" {
-  bucket = "ecommerce-devops-demo-site"
-  acl    = "public-read"
+  bucket = "ecommerce-devops-demo-site-12345"
+}
 
-  website {
-    index_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.website.arn}/*"
+    }]
+  })
 }
