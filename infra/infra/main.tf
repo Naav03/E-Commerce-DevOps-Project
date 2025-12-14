1a51b755b669050a196fa5ec389b6d9fb771d9db
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-14 20:10:37 +0530

    Configure S3 bucket for error document
    Added error document configuration for S3 website.

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -1,15 +1,27 @@
-resource "aws_s3_bucket" "website" {
-  bucket = "ecommerce-devops-demo-site-12345"
+provider "aws" {
+  region = "us-east-1" # change to your desired region
 }
 
+# Create S3 bucket
+resource "aws_s3_bucket" "website" {
+  bucket = "ecommerce-devops-demo-site-12345" # must be globally unique
+  acl    = "public-read"
+}
+
+# Configure bucket for website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website.id
 
   index_document {
     suffix = "index.html"
   }
+
+  error_document {
+    key = "error.html"
+  }
 }
 
+# Remove public access blocks to allow public access
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
 
@@ -19,15 +31,7 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_public_access_block" "website" {
-  bucket = aws_s3_bucket.website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
+# Bucket policy to allow public read access
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
@@ -35,6 +39,7 @@ resource "aws_s3_bucket_policy" "website" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
@@ -43,3 +48,4 @@ resource "aws_s3_bucket_policy" "website" {
     ]
   })
 }
+
